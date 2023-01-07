9f88bb54cdd6a73d23ef92974a6d89667e6a7d5c
hrisikesh-neogi <hrisikesh.neogi@gmail.com>
2023-01-07 19:00:31 +0530

    utils updated

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,8 @@
 import yaml
 from src.constant.training_pipeline import *
 from src.exception import SketchtocodeException
+
+from detectron2.config.config import CfgNode
 import os,sys
 from box import ConfigBox
 
@@ -22,6 +24,16 @@ def get_model_config_attribute():
         return model_config_attributes
     except Exception as e:
         raise SketchtocodeException(e, sys)
+
+def save_model_config_to_yaml_file(model_config:CfgNode, model_config_file_path:str):
+    try:
+        os.makedirs( os.path.dirname(model_config_file_path), exist_ok=True)
+        with open(model_config_file_path, "w") as yaml_file:
+            yaml_file.write(model_config.dump())
+    except Exception as e:
+        raise SketchtocodeException(e, sys)
+
+
 
 
 
