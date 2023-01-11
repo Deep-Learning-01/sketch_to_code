6a6d61e9ca941d393a7ba02a424a1557387393a0
hrisikesh-neogi <hrisikesh.neogi@gmail.com>
2023-01-11 18:59:10 +0530

    added prediction config entity

--- src/entity/prediction_entity/artifact_entity.py
+++ src/entity/prediction_entity/artifact_entity.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass 
+from numpy import array
+
 
 @dataclass 
-class PredictionPipelineArtifact:
-    model_weight_file_path: str
-    model_config_file_path: str
+class DetectionArtifact:
+    obj_det_prediction_boxes: array
+    obj_det_prediction_labels: array
+    text_det_prediction_boxes: array
+    text_det_prediction_labels: array
