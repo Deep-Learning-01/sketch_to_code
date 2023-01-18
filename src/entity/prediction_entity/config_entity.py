b8160bfbfbf936b257639fdc8060336d32b7aaca
hrisikesh-neogi <hrisikesh.neogi@gmail.com>
2023-01-18 18:59:58 +0530

    updated

--- src/entity/prediction_entity/config_entity.py
+++ src/entity/prediction_entity/config_entity.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 from src.constant.prediction_pipeline import *
 from src.constant.training_pipeline import *
-import os,sys
+import os, sys
 from datetime import datetime
 
 from from_root import from_root
@@ -11,74 +11,64 @@ TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 ROOT_DIR: str = from_root()
 
 
-
-@dataclass 
+@dataclass
 class AwsS3Config:
     s3_model_key_path: str = MODEL_TRAINER_TRAINED_MODEL_NAME
-    obj_det_aws_s3_model_dir_path: str = os.path.join( TRAINING_BUCKET_NAME ,
-                                                    MODEL_EVALUATION_S3_OBJ_DET_DIR,
-                                                    MODEL_EVALUATION_S3_SAVED_MODEL_DIR)
+    obj_det_aws_s3_model_dir_path: str = os.path.join(TRAINING_BUCKET_NAME,
+                                                      MODEL_EVALUATION_S3_OBJ_DET_DIR,
+                                                      MODEL_EVALUATION_S3_SAVED_MODEL_DIR)
 
-
-    text_det_aws_s3_model_dir_path: str = os.path.join( TRAINING_BUCKET_NAME,
-                                                    MODEL_EVALUATION_S3_TEXT_DET_DIR,
-                                                    MODEL_EVALUATION_S3_SAVED_MODEL_DIR
-                                                     )
-
+    text_det_aws_s3_model_dir_path: str = os.path.join(TRAINING_BUCKET_NAME,
+                                                       MODEL_EVALUATION_S3_TEXT_DET_DIR,
+                                                       MODEL_EVALUATION_S3_SAVED_MODEL_DIR
+                                                       )
 
 
 aws_s3_config: AwsS3Config = AwsS3Config()
 
-@dataclass
-class PredictionPipelineConfig:
-    artifact_dir: str = os.path.join(ROOT_DIR, ARTIFACT_DIR, PREDICTION_ARTIFACT_DIR,TIMESTAMP)
-   
-
-
-training_pipeline_config: PredictionPipelineConfig = PredictionPipelineConfig()
-
 
 @dataclass
 class PredictionPipelineConfig:
-
-    obj_det_s3_model_path: str = os.path.join( TRAINING_BUCKET_NAME,
-                                            S3_OBJ_DET_DIR,
-                                            MODEL_TRAINER_TRAINED_MODEL_OUTPUT_DIR)
+    artifact_dir: str = os.path.join(ROOT_DIR, ARTIFACT_DIR, PREDICTION_ARTIFACT_DIR, TIMESTAMP)
 
 
-    text_det_s3_model_path: str = os.path.join( TRAINING_BUCKET_NAME,
-                                                S3_TEXT_DET_DIR,
-                                                MODEL_TRAINER_TRAINED_MODEL_OUTPUT_DIR)
+prediction_pipeline_config: PredictionPipelineConfig = PredictionPipelineConfig()
 
 
+@dataclass
+class PredictionPipelineConfig:
+    obj_det_s3_model_path: str = os.path.join(TRAINING_BUCKET_NAME,
+                                              S3_OBJ_DET_DIR,
+                                              MODEL_TRAINER_TRAINED_MODEL_OUTPUT_DIR)
 
-    obj_det_artifact_dir = os.path.join( training_pipeline_config.artifact_dir,
-                                                S3_OBJ_DET_DIR )
+    text_det_s3_model_path: str = os.path.join(TRAINING_BUCKET_NAME,
+                                               S3_TEXT_DET_DIR,
+                                               MODEL_TRAINER_TRAINED_MODEL_OUTPUT_DIR)
 
-    text_det_artifact_dir = os.path.join( training_pipeline_config.artifact_dir,
-                                                S3_TEXT_DET_DIR )
+    obj_det_artifact_dir = os.path.join(prediction_pipeline_config.artifact_dir,
+                                        S3_OBJ_DET_DIR)
 
-
+    text_det_artifact_dir = os.path.join(prediction_pipeline_config.artifact_dir,
+                                         S3_TEXT_DET_DIR)
 
     obj_det_saved_model_dir: str = os.path.join(obj_det_artifact_dir,
-                                            MODEL_TRAINER_TRAINED_MODEL_OUTPUT_DIR ) 
-
-
+                                                MODEL_TRAINER_TRAINED_MODEL_OUTPUT_DIR)
 
     text_det_saved_model_dir: str = os.path.join(text_det_artifact_dir,
-                                            MODEL_TRAINER_TRAINED_MODEL_OUTPUT_DIR ) 
+                                                 MODEL_TRAINER_TRAINED_MODEL_OUTPUT_DIR)
 
-    obj_det_model_weight_path: str = os.path.join( obj_det_saved_model_dir,
-    MODEL_TRAINER_TRAINED_MODEL_NAME)
+    obj_det_model_weight_path: str = os.path.join(obj_det_saved_model_dir,
+                                                  MODEL_TRAINER_TRAINED_MODEL_NAME)
 
-    text_det_model_weight_path: str = os.path.join( text_det_saved_model_dir,
-    MODEL_TRAINER_TRAINED_MODEL_NAME ) 
+    text_det_model_weight_path: str = os.path.join(text_det_saved_model_dir,
+                                                   MODEL_TRAINER_TRAINED_MODEL_NAME)
 
+    obj_det_model_config_path: str = os.path.join(obj_det_saved_model_dir,
+                                                  MODEL_TRAINER_TRAINED_MODEL_CONFIG_FILE_NAME)
 
-    obj_det_model_config_path: str = os.path.join( obj_det_saved_model_dir,
-    MODEL_TRAINER_TRAINED_MODEL_CONFIG_FILE_NAME)
+    text_det_model_config_path: str = os.path.join(text_det_saved_model_dir,
+                                                   MODEL_TRAINER_TRAINED_MODEL_CONFIG_FILE_NAME)
 
-    text_det_model_config_path: str = os.path.join( text_det_saved_model_dir,
-    MODEL_TRAINER_TRAINED_MODEL_CONFIG_FILE_NAME ) 
-
-    
+    html_output_file_path: str = os.path.join(prediction_pipeline_config.artifact_dir,
+                                              HTML_FILE_OUTPUT_DIR,
+                                              HTML_FILE_OUTPUT_NAME)
